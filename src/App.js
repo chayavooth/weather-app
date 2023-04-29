6b954764a9965944abeb8d713240c2a65c6a0cf9
chayavooth <suwantaz1003@hotmail.com>
2023-04-29 22:02:27 +0700

    fix 1

--- src/App.js
+++ src/App.js
@@ -1,74 +1,81 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 
+class WeatherApp extends Component {
+  constructor(props) {
+    super(props);
+    this.API = {
+      key: "38f4667f8a2f51bdff0575d28315049c",
+      base: "http://api.openweathermap.org/data/2.5/"
+    };
+    this.state = {
+      query: "",
+      weather: {}
+    };
+  }
 
-const api = {
-  key: "38f4667f8a2f51bdff0575d28315049c",
-  base: "http://api.openweathermap.org/data/2.5/"
-}
-
-
-
-function App() {
-
-  const dataBuild = (d) => {
+  dataBuild(d) {
     let date = String(new window.Date());
     date = date.slice(3, 15);
     return date;
   }
 
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
-  const search = (e) => {
+  search = (e) => {
     if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+      fetch(`${this.API.base}weather?q=${this.state.query}&units=metric&appid=${this.API.key}`)
         .then((res) => res.json())
         .then((results) => {
-          setQuery("");
-          setWeather(results);
+          this.setState({
+            query: "",
+            weather: results
+          });
           console.log(results);
         })
     }
   }
 
-  return (
-    <div className={
-      typeof weather.main != "undefined" ? weather.main.temp > 18 > 14 ? "App hot" : "App cold" : "App rain"
-    }>
-      <main>
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="search-bar"
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
+  renderWeather() {
+    const { weather } = this.state;
+    return (
+      <div>
+        <div className="location-contanier">
+          <div className="location">
+            {weather.name}, {weather.sys.country}
+          </div>
+          <div className="date">{this.dataBuild(new Date())}</div>
         </div>
-        {typeof weather.main != "undefined" ? (
-        <div>
-          <div className="location-contanier">
-            <div className="location">
-              {weather.name}, {weather.sys.country}
-            </div>
-          <div className="date">{dataBuild(new Date())}</div>
+        <div className="weather-container">
+          <div className="temperature">
+            {Math.round(weather.main.temp)}°C
           </div>
-          <div className="weather-container">
-            <div className="temperature">
-                {Math.round(weather.main.temp)}°C
-            </div>
-              <div className='weather'>{weather.weather[0].main}</div>
-            </div> 
-          </div>
-        ) : (
-          ""
-        )}
-      
-      </main>
-    </div>
-  );
+          <div className='weather'>{weather.weather[0].main}</div>
+        </div>
+      </div>
+    );
+  }
 
+  render() {
+    const { weather } = this.state;
+    return (
+      <div className={
+        typeof weather.main !== "undefined" ? weather.main.temp > 18 > 14 ? "App hot" : "App cold" : "App rain"
+      }>
+        <main>
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-bar"
+              onChange={(e) => this.setState({ query: e.target.value })}
+              value={this.state.query}
+              onKeyPress={this.search}
+            />
+          </div>
+          {typeof weather.main !== "undefined" ? this.renderWeather() : ""}
+        </main>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default WeatherApp;
